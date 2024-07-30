92f80cf704686ad7d01af9a384384165ac533e0f
aixion1506 <aixion1506@gmail.com>
2024-07-30 17:32:29 +0900

    Feat: createBoard - Controller

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -21,4 +21,10 @@ export class BoardsService {
     this.boards.push(board);
     return board;
   }
+  // {
+  //   "id": "09614540-4e4e-11ef-a0b8-97c2ec39f826",
+  //   "title": "Board 1",
+  //   "description": "Description 1",
+  //   "status": "PUBLIC"
+  // }
 }
