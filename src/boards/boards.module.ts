d75518502adcaa3c790cf3ef473c6632956e7263
aixion1506 <aixion1506@gmail.com>
2024-08-04 19:37:31 +0900

    Feat: AuthModule Setting

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { BoardRepository } from './board.repository';
 import { Board } from './board.entity';
 import { BoardRepository } from './board.repository';
 
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -45,10 +45,4 @@ export class BoardsService {
 
     return board;
   }
-
-  // updateBoardStatus(id: string, status: BoardStatus): Board {
-  //   const board = this.getBoardById(id);
-  //   board.status = status;
-  //   return board;
-  // }
 }
